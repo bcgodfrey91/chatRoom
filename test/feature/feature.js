ad4b93a976cdd5eedffa038406cea0492da292a2
Ben Godfrey <bcgodfrey91@gmail.com>
2016-08-17 20:47:08 -0600

    Add input field, submit button, and submit button functionality.

--- test/feature/feature.js
+++ test/feature/feature.js
@@ -25,4 +25,44 @@ describe('attributes of our application', function(){
 
     assert.equal(allIdeas.getText(), 'generally terrible comment')
   })
+
+  it('should set input field back to blank', function(){
+  var userMessage = browser.element('.user-message')
+
+    assert.equal(userMessage.getValue(), '')
+  })
+})
+const assert = require('assert')
+
+describe ('webpage interaction', function(){
+  it('should have a title', function(){
+    browser.url('/');
+    var title = browser.getTitle();
+    assert.equal(title, 'Cat Chat')
+  });
+})
+
+describe('attributes of our application', function(){
+  it('should have an input field that we can type in', function(){
+    browser.url('/');
+    var userMessage = browser.element('.user-message')
+
+    userMessage.setValue('generally terrible comment')
+
+    assert.equal(userMessage.getValue(), 'generally terrible comment')
+  })
+
+  it('should have a button that lets us send that message', function(){
+
+    browser.click('.submit-button')
+    var allIdeas = browser.element('.message')
+
+    assert.equal(allIdeas.getText(), 'generally terrible comment')
+  })
+
+  it('should set input field back to blank', function(){
+  var userMessage = browser.element('.user-message')
+
+  assert.equal(userMessage.getValue(), '')
+  })
 })
