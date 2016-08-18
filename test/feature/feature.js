4e50483b2ca8c1f071d078eb3aa1d6744b577872
Ben Godfrey <bcgodfrey91@gmail.com>
2016-08-17 22:18:18 -0600

    Change ul to a section and refactor function to disable submit button.

--- test/feature/feature.js
+++ test/feature/feature.js
@@ -20,41 +20,7 @@ describe('attributes of our application', function(){
 
   it('should have a button that lets us send that message', function(){
 
-    browser.click('.submit-button')
-    var allIdeas = browser.element('.message')
-
-    assert.equal(allIdeas.getText(), 'generally terrible comment')
-  })
-
-  it('should set input field back to blank', function(){
-  var userMessage = browser.element('.user-message')
-
-    assert.equal(userMessage.getValue(), '')
-  })
-})
-const assert = require('assert')
-
-describe ('webpage interaction', function(){
-  it('should have a title', function(){
-    browser.url('/');
-    var title = browser.getTitle();
-    assert.equal(title, 'Cat Chat')
-  });
-})
-
-describe('attributes of our application', function(){
-  it('should have an input field that we can type in', function(){
-    browser.url('/');
-    var userMessage = browser.element('.user-message')
-
-    userMessage.setValue('generally terrible comment')
-
-    assert.equal(userMessage.getValue(), 'generally terrible comment')
-  })
-
-  it('should have a button that lets us send that message', function(){
-
-    browser.click('.submit-button')
+    browser.click('.send-button')
     var allIdeas = browser.element('.message')
 
     assert.equal(allIdeas.getText(), 'generally terrible comment')
@@ -65,4 +31,10 @@ describe('attributes of our application', function(){
 
   assert.equal(userMessage.getValue(), '')
   })
+
+  it('should disable the send button if user has not typed anything', function(){
+
+  })
+
+  
 })
