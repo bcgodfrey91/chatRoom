e70da6adc2df593a68484d6b2811cc68acf8cd8c
Ben Godfrey <bcgodfrey91@gmail.com>
2016-08-20 21:22:40 -0600

    Edit unit tests to make them functional.

--- test/unitTest/chatroom-test.js
+++ test/unitTest/chatroom-test.js
@@ -27,28 +27,25 @@ describe ('Chatroom', function(){
     var chatroom = new Chatroom();
     var message = new Message({id: 'id', user: 'user', text: 'text'})
 
-    chatroom.createMessage(message)
+    chatroom.createMessage(0, 'bob', 'wow', 'hello')
 
-    assert.equal(chatroom.messages.length, 1)
+    assert.equal(chatroom.messages.length, 2)
   })
 
   it('should let a message maintain its properties once it is placed into the array', function(){
     var chatroom = new Chatroom();
-    var message = new Message({id: 'id', user: 'user', text: 'text'})
 
-    chatroom.createMessage(message)
+    chatroom.storeMessages(0, 'bob', 'wow', 'hello')
 
-    assert.equal(chatroom.messages[0].id, 'id')
-    assert.equal(chatroom.messages[0].user, 'user')
-    assert.equal(chatroom.messages[0].text, 'text')
+    assert.equal(chatroom.messages[0].id, 0)
+    assert.equal(chatroom.messages[0].user, 'bob')
+    assert.equal(chatroom.messages[0].text, 'wow')
   })
 
   it('should be able to store messages in local storage', function(){
     var chatroom = new Chatroom();
-    var message = new Message({id: 'id', user: 'user', text: 'text'});
 
-    chatroom.createMessage(message);
-    chatroom.storeMessages(message);
+    chatroom.storeMessages(0, 'bob', 'wow', 'hello');
 
     var storedMessages = JSON.parse(localStorage.getItem('messages'));
 
@@ -57,10 +54,8 @@ describe ('Chatroom', function(){
 
   it('should be able to retreive messages from local storage', function(){
     var chatroom = new Chatroom();
-    var message = new Message({id: 'id', user: 'user', text: 'text'});
 
-    chatroom.createMessage(message);
-    chatroom.storeMessages(message);
+    chatroom.storeMessages(0, 'bob', 'wow', 'hello');
 
     var storedMessages = chatroom.retrieveMessages();
 
@@ -69,26 +64,21 @@ describe ('Chatroom', function(){
 
   it('should be able to delete the message from local storage', function(){
     var chatroom = new Chatroom();
-    var message1 = new Message({id: 0, user: "Sah", text: "Duh"});
-    var message2 = new Message({id: 1, user: "Ahh", text: "Sah"});
 
-    chatroom.createMessage(message1);
-    chatroom.createMessage(message2);
+    chatroom.createMessage(0, 'andy', 'wow', 'hello');
+    chatroom.createMessage(1, 'ben', 'howdy', 'sah duh');
     chatroom.storeMessages();
 
-    console.log(chatroom.messages);
-
-    chatroom.removeMessageFromStorage(message1.id)
+    chatroom.removeMessageFromStorage(0)
     chatroom.retrieveMessages();
 
-    assert.equal(chatroom.messages[0].id, message2.id);
+    assert.equal(chatroom.messages[0].id, 1);
   })
 
   it('should allow the user to edit their previous messages', function(){
     var chatroom = new Chatroom();
-    var message = new Message({id: 0, user: 'Bandy', text: 'suh duh'});
 
-    chatroom.createMessage(message);
+    chatroom.createMessage(0, 'bob', 'wow', 'hello');
     chatroom.storeMessages();
 
     var editedMessage = new Message({id: 0, user: 'Bandy', text: 'asuhduh'})
